29975285aff97a5ea2b314549c64fdf3d5c469f7
edwinkim16 <edwinkimathi226@gmail.com>
2023-03-15 21:51:32 +0300

    jquerying the icons to toggle and hide

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#icon1").click(function(event){
+    event.preventDefault();
+    $("#icon1").toggle();
+    $("#design").toggle();
+    $("#designtitle").show();
+    });
+
+    //hiding
+    $("p#design").click(function(event){
+        event.preventDefault();
+        $("#icon1").toggle();
+        $("#design").toggle();
+        $("#design").hide();
+        });
+
+                     //icon2
+    $("#icon2").click(function(){
+        $("#icon2").toggle();
+        $("#develop").toggle();
+        $("#developtitle").show();
+    });
+        //hiding
+    $("p#develop").click(function(){
+            $("#icon2").toggle();
+            $("#develop").toggle();
+            $("#develop").hide();
+        });
+                      //icon3
+    $("#icon3").click(function(){
+        $("#icon3").toggle();
+        $("#product").toggle();
+        $("#producttitle").show();
+    });
+    //hiding
+    $("p#product").click(function(){
+        $("#icon3").toggle();
+        $("#product").toggle();
+        $("#product").hide();
+    });
+});
