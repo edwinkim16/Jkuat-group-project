0dbbdffc9b7468cce98ee0c4adffbb32a5194b23
edwinkim16 <edwinkimathi226@gmail.com>
2023-03-15 22:26:17 +0300

    validating contact form

--- script.js
+++ script.js
@@ -39,3 +39,27 @@ $(document).ready(function(){
         $("#product").hide();
     });
 });
+
+//form validation
+function validate(){
+    var username=document.getElementById("name1").value;
+    var email=document.getElementById("email1").value;
+    var message=document.getElementById("messo").value;
+
+    if(username==''){
+        alert('enter your name');
+        username.focus();
+        return false;
+        
+    }else if(email==''){
+        alert('enter your email address');
+        email.focus();
+        return false
+    }else if(message==''){
+        alert('enter your message')
+        message.focus()
+        return false;
+        
+    }else
+    alert('Dear '+username+'\n  your message has been received.')
+}
